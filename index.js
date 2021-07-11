311aa344db082da12f906fb5dd11c51c6c4d2d87
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-11 14:41:04 -0400

    main menu movedcreated as a seperate array

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const inquirer = require('inquirer');
 
+const mainMenu = [
+    'View All Employees',
+    'View All Employees By Department',
+    'View All Employees By Manager',
+    'Add Employee',
+    'Remove Employee',
+    'Update Employee Role',
+    'Update Employee Manager',
+    'View All Roles',
+    'Add Role',
+    'Remove Role',
+]
+
 inquirer
     .prompt([
         {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices:[
-                'View All Employees',
-                'View All Employees By Department',
-                'View All Employees By Manager',
-                'Add Employee',
-                'Remove Employee',
-                'Update Employee Role',
-                'Update Employee Manager',
-                'View All Roles',
-                'Add Role',
-                'Remove Role',
-            ]
-
+            choices: mainMenu
         }
     ])
     .then(answer=>console.log(answer))
