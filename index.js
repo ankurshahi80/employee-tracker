eae9743b9ef844bb86b4226c7063e6d2a0967f6f
Ankur Shahi <ankurshahi@MacBook-Air.phub.net.cable.rogers.com>
2021-07-12 02:36:01 -0400

    add operations completed

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 
-const db = mysql.createConnection(
+const con = mysql.createConnection(
     {
         user:'root',
         password:'Nyr@2021',
-        database:'organization'
+        database:'organization',
     },
     console.log('Connected to the organization database.')
 );
@@ -44,12 +44,125 @@ const promptUser = () => {
     .catch(error=>console.log(error)); 
 }
 
+const addDept = ()=>{
+    return inquirer.prompt(
+        [
+            {
+                type:'input',
+                name:'name',
+                message: 'Enter the department name to add:'
+            }
+        ]
+    );
+}
+
+const addRole = ()=>{
+    const deptArray = [];
+    con.query(`SELECT * FROM department;`,(err,rows)=>{
+        for (let i=0;i<rows.length;i++){
+            deptArray.push(rows[i].name);
+        }
+    });    
+    return inquirer.prompt(
+        [
+            {
+                type:'input',
+                name:'name',
+                message: 'Enter the role name to add:'
+            },
+            {
+                type:'input',
+                name: 'salary',
+                message: 'Enter the salary for the role:'
+            },
+            {
+                type:'list',
+                name:'department',
+                message: 'Choose a department:',
+                choices: deptArray
+            }
+        ]
+    );
+}
+
+getDeptID = (answer)=> {
+    con.promise().query(`SELECT * FROM department WHERE name=?`,answer.department)
+    .then(([rows,fields])=>{
+        const dept = rows[0].id;
+        const params=[answer.name,answer.salary,dept]
+        console.log(params);
+        con.query(`INSERT INTO role (title,salary,department_id) VALUES (?,?,?);`,params ,(err,rows)=>{
+            promptUser();
+        });
+    }); 
+};
+
+const addEmployee = ()=>{
+    const roleArray = [];
+    const managerArray =[];
+    con.query(`SELECT title FROM role;`,(err,rows)=>{
+        for (let i=0;i<rows.length;i++){
+            roleArray.push(rows[i].title);
+        }
+    }); 
+    con.query(`SELECT CONCAT(first_name,' ',last_name) AS name FROM employee;`,(err,rows)=>{
+        for (let i=0;i<rows.length;i++){
+            managerArray.push(rows[i].name);
+        }
+    });     
+    return inquirer.prompt(
+        [
+            {
+                type:'input',
+                name:'first_name',
+                message: "Enter the employee's first name:"
+            },
+            {
+                type:'input',
+                name:'last_name',
+                message: "Enter the employee's last name:"
+            },
+            {
+                type:'list',
+                name:'role',
+                message: 'Choose a role:',
+                choices: roleArray
+            },
+            {
+                type:'list',
+                name:'manager',
+                message: 'Choose a manager:',
+                choices: managerArray
+            }
+        ]
+    );
+}
+
+getEmployee = (answer)=> {
+    console.log(answer);
+    con.promise().query(`SELECT * FROM role WHERE title=?`,answer.role)
+    .then(([rows,fields])=>{
+        const role = rows[0].id;
+        let managerParams=answer.manager.split(" ");
+        console.log(managerParams);
+        con.promise().query(`SELECT * FROM employee WHERE ((first_name = ?) AND (last_name = ?))`,managerParams)
+        .then(([rows,field])=>{
+            console.log(rows);
+            const manager = rows[0].id;
+            const params=[answer.first_name,answer.last_name,role,manager]
+            console.log(params);
+            con.query(`INSERT INTO employee (first_name,last_name,role_id, manager_id) VALUES (?,?,?,?);`,params ,(err,rows)=>{
+                promptUser();
+            });
+        });
+    });  
+};
 const chooseCase = (answer)=>{
     let choice = answer.choice;
     switch(choice) {
         case "View All Departments":
         
-        db.query(`SELECT id,department.name as department
+        con.query(`SELECT id,department.name as department
         FROM department`,(err,rows)=>{
             console.table(rows);
             promptUser();
@@ -57,7 +170,7 @@ const chooseCase = (answer)=>{
         break;
 
         case "View All Roles":
-        db.query(`SELECT role.id as 'role id', role.title as 'job title', department.name as department, role.salary
+        con.query(`SELECT role.id as 'role id', role.title as 'job title', department.name as department, role.salary
         FROM role
         INNER JOIN department ON department.id =role.department_id;`,(err,rows)=>{
             console.table(rows);
@@ -66,23 +179,37 @@ const chooseCase = (answer)=>{
         break;
         case "View All Employees":
         
-        db.query(`SELECT employee.id,employee.first_name,employee.last_name,role.title as 'job title',department.name as department, role.salary, CONCAT(e.first_name,' ',e.last_name) AS manager
+        con.query(`SELECT employee.id,employee.first_name,employee.last_name,role.title as 'job title',department.name as department, role.salary, CONCAT(e.first_name,' ',e.last_name) AS manager
         FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department on department.id = role.department_id  LEFT JOIN employee e ON employee.manager_id=e.id ORDER BY id;`,(err,rows)=>{
             console.table(rows);
             promptUser();
         });
         break;
 
-        case "View All Employees By Department":
-        console.log("View All Employees By Department");
+        case "Add A Department":
+            addDept().then(answer=>{
+                console.log(answer.name);
+                con.query(`INSERT INTO department (name) VALUES (?);`,answer.name ,(err,rows)=>{
+                    console.table(rows);
+                    promptUser();
+                });
+            });
+        break;
+
+        case "Add A Role":
+            addRole().then(answer=>{
+                getDeptID(answer);
+            });
+        break;
+
+        case "Add An Employee":
+            addEmployee().then(answer=>{
+                getEmployee(answer);
+            })
         break;
 
         case "View All Employees By Manager":
         console.log("View All Employees By Manager");
-        break;
-
-        case "Add Employee":
-        console.log("Add Employee");
         break;
 
         case "Remove Employee":
@@ -97,10 +224,6 @@ const chooseCase = (answer)=>{
         console.log("Update Employee Manager");
         break;
 
-        case "Add Role":
-        console.log("Add Role");
-        break;
-
         case "Remove Role":
         console.log("Remove Role");
         break;
@@ -110,4 +233,4 @@ const chooseCase = (answer)=>{
     }
 }
 
-promptUser()
+promptUser();
